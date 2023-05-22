6d1011529726621f756637db36dce05f46c56195
omar <omar-malik22@hotmail.com>
2023-05-22 02:09:59 +0100

    simplified tailwind CSS classes

--- src/components/Navbar/MenuButton.jsx
+++ src/components/Navbar/MenuButton.jsx
@@ -1,6 +1,6 @@
 function MenuButton() {
   return (
-    <button type="button" className="text-gray-500 hover:text-gray-700 focus:outline-none mr-4">
+    <button type="button" className="text-gray-500 hover:text-gray-700 focus:outline-none md:hidden">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-6 h-6">
         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.5h118M3.75 13h16.5m-16.5 6h20" />
       </svg>
