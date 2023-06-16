e7a53fe8252b0279541c4da6d7ac6ddd8d75b18a
omar <omar-malik22@hotmail.com>
2023-06-16 19:49:55 +0100

    started search component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,18 +5,22 @@ import { useState } from "react";
 function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
   const fragrance = (
-    <div onMouseOver={() => setOpen(true)} onMouseLeave = {() => setOpen(false)}className="flex">
+    <div
+      onMouseOver={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+      className="flex"
+    >
       FRAGRANCE
       <svg
-        class="ml-0 -mr-0. h-4 w-4"
+        className="ml-0 -mr-0. h-4 w-4"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 20 20"
         fill="currentColor"
       >
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M6.293 7.293a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-          clip-rule="evenodd"
+          clipRule="evenodd"
         />
       </svg>
     </div>
@@ -25,7 +29,7 @@ function Navbar() {
     <div className="flex">
       GIFTS
       <svg
-        class="ml-0 -mr-0. h-4 w-4"
+        className="ml-0 -mr-0. h-4 w-4"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 20 20"
         fill="currentColor"
@@ -65,7 +69,6 @@ function Navbar() {
   const [open, setOpen] = useState(false);
   return (
     <>
-
       <div className="flex justify-center w-full flex-col items-center">
         <nav className="flex justify-between w-full max-w-full p-3">
           <div className="p-0 pt-5">
@@ -112,31 +115,38 @@ function Navbar() {
             <ShoppingBasket />
           </div>
         </nav>
+
         <div className="pt-4 lg:flex gap-5 text-xs tracking-widest hidden">
-          {links.map((link, i) => {
-            return <a key={i} className="hover:text-gold transition duration-300" href="/">{link}</a>;
-          })}
-        </div>
-      </div>
-      
-      {open == true && (
-        <div onMouseOver = {() => setOpen(true)} onMouseLeave = {() => setOpen(false)} className="bg-white w-full h-[550px] p-7">
           {links.map((link, i) => {
             return (
               <a
                 key={i}
+                className="hover:text-gold transition duration-300"
                 href="/"
-                className=""
               >
                 {link}
               </a>
             );
           })}
         </div>
-      )
+      </div>
 
-      }
-
+      {open == true && (
+        <div
+          id="navbar-fragrance-dropdown"
+          onMouseOver={() => setOpen(true)}
+          onMouseLeave={() => setOpen(false)}
+          className="bg-white w-full h-[550px] p-7"
+        >
+          {links.map((link, i) => {
+            return (
+              <a key={i} href="/" className="">
+                {link}
+              </a>
+            );
+          })}
+        </div>
+      )}
 
       {showMenu == true && (
         <div className="flex flex-col bg-white h-full w-[320px] fixed top-0 left-0">
@@ -175,6 +185,36 @@ function Navbar() {
           })}
         </div>
       )}
+
+      <div className="bg-white h-full w-[320px] absolute right-0">
+        <div className="flex">
+        <h1 className="p-5 tracking-wider">SEARCH OUR SITE</h1>
+        <button className="">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+        </div>
+
+        <div className="flex p-7 border ">
+          <input
+            className="w-[265px] font-light text-xs h-[40px] p-3 border"
+            placeholder="Search for products"
+            type="text"
+          />
+        </div>
+      </div>
     </>
   );
 }
