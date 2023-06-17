2e8dbf8acda48a4af22e48d7fc3daa325a4fad9c
omar <omar-malik22@hotmail.com>
2023-06-18 00:53:35 +0100

    changed search element to button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -58,7 +58,7 @@ function Navbar() {
             </button>
           </div>
 
-          <div className="pt-2 absolute left-1/2 -translate-x-1/2 ">
+          <div className="pt-1 absolute left-1/2 -translate-x-1/2 ">
             <a className="" href="/">
               <div className="text-3xl text-gold font-light tracking-wide">
                 SCENTEDMUSK
@@ -69,10 +69,10 @@ function Navbar() {
             </a>
           </div>
 
-          <div className="flex space-x-4 p-2 pt-5">
-            <a
+          <div className="flex space-x-4 p-2 pt-4">
+            <button
               onClick={()=> setShowSearch(true)}
-              className="hover:text-gold transition duration-200 focus:outline-none"
+              className="w-6 h-6 hover:text-gold transition duration-200 focus:outline-none"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -88,7 +88,7 @@ function Navbar() {
                   d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
                 />
               </svg>
-            </a>
+            </button>
             <ShoppingBasket />
           </div>
         </nav>
