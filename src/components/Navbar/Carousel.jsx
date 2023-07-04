847dab7545b0df31ae7107f3fbb976a7ea7c6330
omar <omar-malik22@hotmail.com>
2023-07-04 18:45:52 +0100

    added tag and text to carousel

--- src/components/Navbar/Carousel.jsx
+++ src/components/Navbar/Carousel.jsx
@@ -1,23 +1,36 @@
 import { useState, useEffect } from "react";
 
-const Carousel = ( {images} ) => {
-  const [currentImage, setCurrentImage] = useState(0)
+const Carousel = ({ images }) => {
+  const [currentImage, setCurrentImage] = useState(0);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setCurrentImage((prevImage) => (prevImage + 1) % images.length)
-    }, 10000)
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }, 10000);
 
     return () => {
-      clearTimeout(timer)
-    }
-  }, [currentImage, images])
+      clearTimeout(timer);
+    };
+  }, [currentImage, images]);
 
   return (
-    <div className=" bg-white h-[574px] w-full">
-      <img src={images[currentImage]} alt="carousel" className="w-full h-full object-cover" />
-    </div>
-  )
-}
+    <>
+      <div className="relative bg-white h-[574px] w-full md:h-[50vw] lg:h-[700px]">
+        <img
+          src={images[currentImage]}
+          alt="carousel"
+          className="w-full h-full object-cover object-top"
+        />
+        <div className="flex flex-col items-center justify-cente absolute z-30 text-white bottom-10 w-full p-7 ">
+          <div className=" flex flex-col items-center">
+            <div className="font-light tracking-[1px]">PREMIUM FRAGRANCES</div>
+            <div className="font-thin text-sm p-2">DISCOVER YOUR SCENT</div>
+            <a href="" className="bg-white text-sm text-black p-2 pl-10 pr-10 rounded-full">Shop Now</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Carousel;
