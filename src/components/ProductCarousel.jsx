78c8e8b2c432238097f04fc2d679043bd876d4b7
omar <omar-malik22@hotmail.com>
2023-07-12 01:10:13 +0100

    added more styling to product carousel

--- src/components/ProductCarousel.jsx
+++ src/components/ProductCarousel.jsx
@@ -8,7 +8,7 @@ import "swiper/css/pagination";
 const ProductCarousel = () => {
   return (
     <>
-      <div className="h-[542px] w-[542px]">
+      <div className="h-[642px] w-[542px]">
         <div className="h-20 w-full flex flex-col items-center justify-center tracking-[2px] font-light text-lg">
           <div className="">EXPLORE EVOCATIVE SCENTS</div>
         </div>
@@ -20,16 +20,16 @@ const ProductCarousel = () => {
           pagination={{ clickable: true }}
           slidesPerView={1}
           spaceBetween={0}
-          className=""
+          className="h-full w-full"
         >
           {products.map((product, i) => {
             return (
               <div key={i} className="h-full w-full">
                 <SwiperSlide className="">
-                  <div className=" font-light ">
+                  <div className="space-y-1 font-light ">
                     <img className="border-[1px]" src={product.image} alt="" />
-                    <div className="">{product.name}</div>
-                    <div className="">Intensity {product.intensity}</div>
+                    <div className=" font-medium tracking-[1px]">{product.name}</div>
+                    <div className="font-light">Intensity {product.intensity}</div>
                     <div className="text-sm text-gray-500">
                       £{product.price["10ml"]} - £{product.price["50ml"]}
                     </div>
