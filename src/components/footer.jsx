7150260cf865a0781dee0e7b8d98e0d401fca48d
omar <omar-malik22@hotmail.com>
2023-07-25 18:45:53 +0100

    added credits to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,18 +5,22 @@ import Payment from "../assets/Payment";
 const Footer = () => {
   return (
     <div>
-      <div className="  flex flex-col  h-[365px] bg-black text-white">
-        <div className="p-8 flex h-[350px] flex-col justify-between">
-          <p className="pl-1">
+      <div className="  flex flex-col h-[365px] bg-black text-white">
+        <div className="p-6 flex h-[350px] flex-col justify-between">
+          <p className="">
             Images found on{""}
             <a
-              className="p-1 hover:bg-slate-700 underline hover:underline-offset-2 transition duration-00 rounded-md"
+              className="p-1 hover:bg-slate-700 underline hover:underline-offset-2 transition duration-100 rounded-md"
               href="https://www.freepik.com/free-photo/healthy-argan-oil-arrangement_14959159.htm#from_view=detail_serie#position=6"
             >
               Freepik.com
             </a>
             {""}by artists:
           </p>
+          <div className="mt-[-30px] flex flex-row justify-between w-[300px] text-gold">
+          <a className="hover:bg-slate-700 underline hover:underline-offset-2 transition duration-100 rounded-md" href="https://www.freepik.com/author/xvector">xvector on freepik</a>
+          <a className="hover:bg-slate-700 underline hover:underline-offset-2 transition duration-100 rounded-md" href="https://www.freepik.com/author/vecstock">vecstock on freepik</a>
+          </div>
 
           <p className="flex flex-row">
             <Truck />{" "}
