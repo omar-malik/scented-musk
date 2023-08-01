72bd32122cda4e54168f636a0af0a68a051fd1e2
omar <omar-malik22@hotmail.com>
2023-08-01 02:38:23 +0100

    added some styling to the product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,30 +2,23 @@ import { productData } from "../newProductData";
 import { useParams } from "react-router-dom";
 
 const Product = () => {
+  const productId = `${useParams().productId}`;
+  const currentProduct = productData[productId];
 
-  const productId = `${useParams().productId}`
-  const currentProduct = productData[productId]
-  
-
-  console.log(productData[productId])
+  console.log(productData[productId]);
   return (
-    
-    <div className="flex flex-col justify-content p-4 relative pt-[100px]">
+    <div className="flex flex-col justify-content p-4 relative pt-[100px] bg-gray-300">
       <div>
-        <img className="h=[300px] w-[300px]" src={currentProduct.image} alt="" />
+        <img className="h=[full] w-[full]" src={currentProduct.image} alt="" />
       </div>
-      <div>
-        {currentProduct.name}
+
+      <div className="flex flex-col gap-3 pt-3">
+        <div className="font-fine">{currentProduct.name}</div>
+        <div>£{currentProduct.price["10ml"]}</div>
+        <div>intensity {currentProduct.intensity}</div>
+        <div>{currentProduct.description}</div>
       </div>
-      <div>
-        £{currentProduct.price["10ml"]}
-      </div>
-      <div>
-        intensity {currentProduct.intensity}
-      </div>
-      <div>
-        {currentProduct.description}
-      </div>
+
     </div>
   );
 };
