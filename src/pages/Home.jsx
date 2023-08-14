50bb63ba8962d1c3ebc700b3929d67529371c556
omar <omar-malik22@hotmail.com>
2023-08-14 23:17:15 +0100

    edited homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,25 @@
 import Carousel from "../components/Carousel";
-import image1 from "../assets/images/table.png"
-import image2 from "../assets/images/green-background.png"
-import ProductCarousel from "../components/ProductCarousel"
-
+import image1 from "../assets/images/table.png";
+import image2 from "../assets/images/green-background.png";
+import ProductCarousel from "../components/ProductCarousel";
 
 function HomePage() {
-const images = [
-  image1, image2
-]
+  const images = [image1, image2];
   return (
     <div className="bg-gray-100 h-full flex flex-col items-center">
       <Carousel images={images} />
-<ProductCarousel></ProductCarousel>
-     <div className="flex flex-col sm:flex-row lg:gap-[230px] sm:items-center justify-center gap-2 bg-neutral-200 p-5 w-full h-[200px]">
-      <p className="text-xl md:font-light md:text-3xl sm:w-[300px] md:w-[450px] font-[quicksand] w-[240px]">
-        EXPLORE OUR LUXURY PERFUME COLLECTIONS
-      </p>
-      <p className="font-thin text-sm sm:w-[350px] md:w-[400px] md:text-lg">
-      Experiencing our fragrances just got easier! Try our scents from our new collection.
-      </p>
-      </div> 
+      <ProductCarousel></ProductCarousel>
+      <div>
+        <div className="flex flex-col sm:flex-row lg:gap-[230px] sm:items-center justify-center gap-2 bg-neutral-200 p-5 w-full h-[200px]">
+          <p className="text-xl md:font-light md:text-3xl sm:w-[300px] md:w-[450px] font-[quicksand] w-[240px]">
+            EXPLORE OUR LUXURY PERFUME COLLECTIONS
+          </p>
+          <p className="font-thin text-sm sm:w-[350px] md:w-[400px] md:text-lg">
+            Experiencing our fragrances just got easier! Try our scents from our
+            new collection.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
